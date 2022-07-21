3d7f58c8875c8d173c47cf6895ada0e10d730975
wyreinec <mariayo5030@gmail.com>
2022-07-21 18:16:15 +0700

    first commit

---
+++ src/problemset2/pkg2/ProblemSet22.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package problemset2.pkg2;
+
+/**
+ *
+ * @author FGA Yohana Sri Rejeki
+ */
+public class ProblemSet22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("   ___         ___  "); 
+        System.out.println("  |   \\_______/   |  ");
+        System.out.println("  |               | ");
+        System.out.println("   \\  --      -- /");
+        System.out.println("    |   /\\   X  |");
+        System.out.println("    | ====o==== |");
+        System.out.println("     \\   _|_    /  ");
+        System.out.println("      \\________/   ");
+    }
+    
+}
